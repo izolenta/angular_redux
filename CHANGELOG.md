c883ccd1981f217b0b044c49ae357dcba4163fca
alexandr.polyankin <alexandr.polyankin@team.wrike.com>
2020-02-29 15:16:36 +0300

    Add SelectPipe

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 1.1.0
+
+  * Add SelectPipe
+
+
 # 1.0.0
 
   * Initial release
---
+++ lib/src/select_pipe.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:angular/angular.dart';
+import 'package:redux/redux.dart';
+
+@Pipe('select', pure: false)
+class SelectPipe<T> implements PipeTransform, OnDestroy {
+  final Store<T> _store;
+  final ChangeDetectorRef _detector;
+
+  dynamic _value;
+  dynamic Function(T state) _selector;
+  StreamSubscription<T> _subscription;
+
+  SelectPipe(this._store, this._detector);
+
+  dynamic transform(dynamic Function(T state) selector) {
+    _selector = selector;
+    _value = selector(_store.state);
+    _subscription ??= _store.onChange.listen(_onStateChange);
+
+    return _value;
+  }
+
+  @override
+  void ngOnDestroy() {
+    _subscription?.cancel();
+  }
+
+  void _onStateChange(T state) {
+    final dynamic value = _selector(state);
+
+    if (value != _value) {
+      _value = value;
+      _detector.markForCheck();
+    }
+  }
+}
